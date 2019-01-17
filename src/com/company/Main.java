9c5370b6699f1a1d6b36e139dd15b386a24de89e
Jacob Brown <jacobbrown2005@gmail.com>
2019-01-17 15:52:22 -0600

    Initial check.

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args){
+        int number1 = 3;
+        int number2 = 6;
+        boolean number1Odd = number1 % 2 == 1;
+        boolean number2Odd = number2 % 2 == 1;
+        if(number1Odd){
+            System.out.println("true");
+
+            }
+        }
+
+
+    }
+
+}
